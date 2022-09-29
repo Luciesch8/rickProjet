b93a020d412e6d5a5001eeb25ed32db9e008476a
Lucie <luciesch8@gmail.com>
2022-09-29 10:41:06 +0200

    Initial Commit

---
+++ rickProjet/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  rickProjet
+//
+//  Created by Ah lucie nous gênes 🍄 on 29/09/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
